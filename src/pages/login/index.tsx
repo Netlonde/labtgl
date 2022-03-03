5aacb445752585e651167371414033fd9822e1c9
Netlonde <carloscomercialti@gmail.com>
2022-03-03 09:51:20 -0300

    initial commit 2.0

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LoginContainer from "./styled";
-import Slogan from "../../components/slogan/Slogan";
+import Slogan from "@components/slogan/Slogan";
 
 function Login(){
   return(
     <>
-
+      <Slogan />
       <LoginContainer>
           Login
       </LoginContainer>
